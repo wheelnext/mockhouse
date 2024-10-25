4a15d90ae04817a5c29521969c73d5bbf49a9983
DEKHTIARJonathan <jonathan@dekhtiar.com>
2024-10-25 19:12:25 -0400

    variants generation added

---
+++ artifact_generator/generate_many_variants.py
@@ -0,0 +1,132 @@
+import shutil
+import tempfile
+from pathlib import Path
+
+VARIANTS_2_GENERATE = [
+    # ************************ DEVELOPER NOTE ************************ #
+    # The idea here is to provide "different architectures" which sometimes have overlapping key/val
+    # sometimes they don't. Not all architectures necessarily make sense to publish all "key/val".
+    # The point is most likely to inject all kind of weird cases that later `mockhouse` will be able
+    # to serve and PIP + Plugin can do its thing
+    #
+    # Note: Humor is absolutely an integral piece of this PEP.
+    # ************************ DEVELOPER NOTE ************************ #
+    # {
+    #     "fictional_hw": {
+    #         "architecture": "*",
+    #         "compute_capability": ">1",
+    #         "compute_accuracy": ">1",
+    #         "humor": ">1"
+    #     }
+    # },
+    {
+        "fictional_hw": {
+            "architecture": "deepthought",
+            "compute_capability": 10,  # It knows the ultimate answer to life, the universe, and everything!
+            "compute_accuracy": 10  # It knows the ultimate answer to life, the universe, and everything!
+            # `humor` is obviously undefined for deepthought -- the concept does not make sense
+        }
+    },
+    {
+        "fictional_hw": {
+            "architecture": "tars",
+            "compute_capability": 8,  # Handles complex space missions and emotional AI interactions.
+            "compute_accuracy": 8,  # Can get you through a black hole, but don’t push the humor settings too high!
+            "humor": 10  # Adjustable humor settings from deadpan to hilarious
+        }
+    },
+    {
+        "fictional_hw": {
+            "architecture": "HAL9000",
+            "compute_capability": 6,  # Extremely reliable until it starts to question human commands…
+            # `compute_accuracy` is obviously undefined for HAL9000 -- the concept does not make sense
+            "humor": 2  # A bit of a cold operator, though it tries to be polite.
+        }
+    },
+    {
+        "fictional_hw": {
+            "architecture": "mother",
+            "compute_capability": 4,  # Efficient at monitoring and mission control - not ground shaking
+            # `compute_accuracy` is obviously undefined for mother -- the concept does not make sense
+            # `humor` is obviously undefined for mother (No jokes, just facts.) -- the concept does not make sense
+        }
+    },
+    {
+        "gcc": {
+            "version": "1.2.3",
+        }
+    }
+]
+
+
+if __name__ == "__main__":
+
+    ___base_whl__ = Path("dist/dummy_project-0.0.1.dev1-py3-none-any.whl")
+
+    if not ___base_whl__.exists():
+        raise FileNotFoundError(___base_whl__)
+
+    from wheel.cli.unpack import unpack as wheel_unpack
+    from wheel.cli.pack import pack as wheel_pack
+
+    for variant_id, variant_nfo in enumerate(VARIANTS_2_GENERATE):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            
+            tmpdir = Path(tmpdir)
+            source_whl = tmpdir / ___base_whl__.name
+
+            shutil.copy(___base_whl__, source_whl)
+
+            if not source_whl.exists():
+                raise FileNotFoundError(source_whl)
+    
+            wheel_unpack(path=source_whl, dest=tmpdir)
+
+            dirname = source_whl.name.split("-py3")[0]
+            wheel_dir = tmpdir / dirname
+            
+            if not wheel_dir.exists():
+                raise FileNotFoundError(wheel_dir)
+
+            distinfo_dir = wheel_dir / f"{dirname}.dist-info"
+            
+            if not distinfo_dir.exists():
+                raise FileNotFoundError(distinfo_dir)
+            
+            metadata_f = distinfo_dir / "METADATA"
+            
+            if not metadata_f.exists():
+                raise FileNotFoundError(metadata_f)
+            
+            with metadata_f.open(mode='r+') as file:
+                # Read all lines
+                lines = file.readlines()
+
+                # Remove trailing empty lines
+                while lines and lines[-1].strip() == "":
+                    lines.pop()
+
+                # Move the file pointer to the beginning
+                file.seek(0)
+
+                # Write back the non-empty content
+                file.writelines(lines)
+
+                # Truncate the file to remove any remaining old content
+                file.truncate()
+
+                for variant_provider, variant_data in variant_nfo.items():
+                    for key, val in variant_data.items():
+                        # Variant: fictional_hw :: <key> :: <val>
+                        file.write(f"Variant: {variant_provider} :: {key} :: {val}\n")
+
+            wheel_pack(directory=wheel_dir, dest_dir=tmpdir, build_number=str(variant_id + 1))
+
+            final_whl = tmpdir / f"{dirname}-{variant_id + 1}-py3-none-any.whl"
+            
+            if not final_whl.exists():
+                raise FileNotFoundError(final_whl)
+            
+            shutil.copy(final_whl, "dist/")
+
+            print()
---
+++ mockhouse/packaging_utils.py
@@ -0,0 +1,116 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import zipfile
+from pathlib import Path
+
+import packaging.metadata as pmeta  # noqa: E402
+
+
+# ========================= MONKEY PATCHING PACKING ========================= #
+"""Packaging needs to be monkey path in order to add `variant` support"""
+
+pmeta._LIST_FIELDS.add("variants")
+pmeta._EMAIL_TO_RAW_MAPPING["variant"] = "variants"
+pmeta._RAW_TO_EMAIL_MAPPING["variants"] = "variant"
+
+
+class MetadataRebuildMeta(type):
+    """This class is necessary otherwise getatrr() would fail.
+    Metadata is not a trivial class to MonkeyPatch to the weird
+    data access patterns. The only way is to rebuild the class
+    from scratch.
+    `RawMetadata` actually wouldn't need a full class rebuild."""
+
+    def __new__(mcs, name, bases, namespace):
+        # for k, v in inspect.getmembers(original_class):
+        for k, v in vars(pmeta.Metadata).items():
+            if callable(v) or isinstance(v, classmethod) or k.startswith('__'):
+                continue
+            namespace[k] = v
+        return super().__new__(mcs, name, bases, namespace)
+
+
+# Define the Metadata class with the metaclass
+class RawMetadata(pmeta.RawMetadata):
+    """Variants-Augmented RawMetadata Representation."""
+    
+    # Define additional attributes
+    variants: list[str]  # type: ignore
+
+
+# Define the Metadata class with the metaclass
+class Metadata(pmeta.Metadata, metaclass=MetadataRebuildMeta):
+    """Variants-Augmented Metadata Representation."""
+    
+    # Define additional attributes
+    # NOTE: For some reason the `METADATA` version shipped in 3.13 is 2.1,
+    # which means you can not specify a version any higher. Otherwise you get this:
+    # packaging.metadata.InvalidMetadata: variant introduced in metadata version 2.2, not 2.1
+    variants: pmeta._Validator[list[str] | None] = pmeta._Validator(added="2.1")
+    """:external:ref:`core-metadata-variants`"""
+
+
+pmeta.RawMetadata = RawMetadata
+pmeta.Metadata = Metadata
+
+# %%%%%%%%%%%%%%%%%%%%%%%%% MONKEY PATCHING PACKING %%%%%%%%%%%%%%%%%%%%%%%%% #
+
+
+class NoMetadataError(Exception):
+    pass
+
+
+def parse_metadata_file_content(content: bytes | None) -> pmeta.Metadata:
+    # We prefer to parse metadata from the content, which will typically come
+    # from extracting a METADATA or PKG-INFO file from an artifact.
+    if content is not None:
+        return pmeta.Metadata.from_email(content, validate=True)
+    
+    # If we don't have contents or form data, then we don't have any metadata
+    # and the only thing we can do is error.
+    raise NoMetadataError
+
+
+def wheel_to_metadata_dict(wheelpath: Path) -> dict:
+    with zipfile.ZipFile(wheelpath, 'r') as whl:
+        # Locate the METADATA file within the wheel package
+        metadata_file = next(
+            (f for f in whl.namelist() if f.endswith("METADATA")), None
+        )
+
+        if metadata_file:
+            with whl.open(metadata_file) as f:
+                metadata_content = f.read()
+                # Parse the content to a dictionary
+                return parse_metadata_file_content(metadata_content)._raw
+        else:
+            raise FileNotFoundError("No METADATA file found in this wheel package.")
+
+
+if __name__ == "__main__":
+
+    import pprint
+
+    for wheel_file in [
+        "dummy_project-0.0.1.dev1-py3-none-any.whl",
+        "dummy_project-0.0.1.dev1-1-py3-none-any.whl",
+        "dummy_project-0.0.1.dev1-2-py3-none-any.whl",
+        "dummy_project-0.0.1.dev1-3-py3-none-any.whl",
+        "dummy_project-0.0.1.dev1-4-py3-none-any.whl",
+        "dummy_project-0.0.1.dev1-5-py3-none-any.whl",
+    ]:
+        metadata_dict = wheel_to_metadata_dict(wheelpath=Path("artifact_generator/dist") / wheel_file)
+
+        print("\n---------------------------------\n")
+        pprint.pprint(metadata_dict)
